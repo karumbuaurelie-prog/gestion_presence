fbc8a3175c7a98380a00002815625b65e4668e17
karumbuaurelie-prog <karumbuaurelie@gmail.com>
2026-08-12 13:27:23 +0200

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -1,13 +1,13 @@
 <?php
-$host = $_ENV['MYSQLHOST'];
-$user = $_ENV['MYSQLUSER'];
-$pass = $_ENV['MYSQLPASSWORD'];
-$db   = $_ENV['MYSQLDATABASE'];
-$port = $_ENV['MYSQLPORT'];
+$host = $_ENV['MYSQLHOST'] ?? 'localhost';
+$user = $_ENV['MYSQLUSER'] ?? 'root';
+$password = $_ENV['MYSQLPASSWORD'] ?? '';
+$dbname = $_ENV['MYSQLDATABASE'] ?? 'railway';
+$port = $_ENV['MYSQLPORT'] ?? 3306;
 
-$conn = new mysqli($host, $user, $pass, $db, $port);
+$conn = new mysqli($host, $user, $password, $dbname, $port);
 
 if ($conn->connect_error) {
-    die("Connexion échouée: " . $conn->connect_error);
+    die("Erreur de connexion: " . $conn->connect_error);
 }
 ?>
