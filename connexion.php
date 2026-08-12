d86868d1f159d68f2b7afa57c13d8e998fe0b1e6
karumbuaurelie-prog <karumbuaurelie@gmail.com>
2026-08-12 15:04:44 +0200

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -2,7 +2,7 @@
 $host = $_ENV['MYSQLHOST'] ?? 'localhost';
 $user = $_ENV['MYSQLUSER'] ?? 'root';
 $password = $_ENV['MYSQLPASSWORD'] ?? '';
-$dbname = $_ENV['MYSQLDATABASE'] ?? 'railway';
+$dbname = $_ENV['MYSQL_DATABASE'] ?? 'railway'; // <- AJOUTE LE _ ICI
 $port = $_ENV['MYSQLPORT'] ?? 3306;
 
 $conn = new mysqli($host, $user, $password, $dbname, $port);
